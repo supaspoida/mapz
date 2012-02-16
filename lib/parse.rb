ae1d1624ece40e4bb4f7d158c3c656bfb21b6899
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2012-02-16 18:52:53 -0500

    Initial parser implementation

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,6 +1,12 @@
 require 'ostruct'
+require 'active_support/core_ext/string/filters'
 
-module Parse
+class Parse < Struct.new(:text)
+
+  def shows
+    text.split("\n").map! &:squish
+  end
+
   class Show < OpenStruct
   end
 end
--- spec/lib/parse_spec.rb
+++ spec/lib/parse_spec.rb
@@ -1,6 +1,35 @@
 require 'spec_helper'
 require 'parse'
 
+describe Parse do
+  describe 'initialization' do
+    let(:text) do
+      %{Bisco |  08/21/03 |  Crocodile Rock |  Allentown, PA |  Setlist
+        Bisco |  08/22/03 |  Bay Center |  Dewey Beach, DE |  Setlist}
+    end
+
+    let(:parser) do
+      described_class.new(text)
+    end
+
+    subject { parser }
+
+    its(:text) { should == text }
+    its(:shows) { should have(2).shows }
+
+    describe '#shows' do
+      subject { parser.shows }
+
+      its(:first) do
+        should == 'Bisco | 08/21/03 | Crocodile Rock | Allentown, PA | Setlist'
+      end
+      its(:last) do
+        should == 'Bisco | 08/22/03 | Bay Center | Dewey Beach, DE | Setlist'
+      end
+    end
+  end
+end
+
 describe Parse::Show do
   let(:attributes) do
     { date: '08/21/03',
