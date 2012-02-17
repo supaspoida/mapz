5a10aa93f728635879b2a9a9e2fdcd31557d8975
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2012-02-17 00:23:50 -0500

    Parse#shows returns array of Show instances

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,12 +1,41 @@
-require 'ostruct'
 require 'active_support/core_ext/string/filters'
 
 class Parse < Struct.new(:text)
 
   def shows
-    text.split("\n").map! &:squish
+    text.split("\n").map &Show.method(:new)
   end
 
-  class Show < OpenStruct
+  class Show < Struct.new(:raw)
+
+    def raw
+      super.squish
+    end
+
+    def date
+      columns[1]
+    end
+
+    def city
+      city_and_state[0]
+    end
+
+    def state
+      city_and_state[1]
+    end
+
+    def venue
+      columns[2]
+    end
+
+    private
+
+    def city_and_state
+      columns[3].split(', ')
+    end
+
+    def columns
+      raw.split(' | ')
+    end
   end
 end
--- spec/lib/parse_spec.rb
+++ spec/lib/parse_spec.rb
@@ -8,38 +8,23 @@ describe Parse do
         Bisco |  08/22/03 |  Bay Center |  Dewey Beach, DE |  Setlist}
     end
 
-    let(:parser) do
-      described_class.new(text)
-    end
-
-    subject { parser }
+    subject { described_class.new(text) }
 
     its(:text) { should == text }
     its(:shows) { should have(2).shows }
-
-    describe '#shows' do
-      subject { parser.shows }
-
-      its(:first) do
-        should == 'Bisco | 08/21/03 | Crocodile Rock | Allentown, PA | Setlist'
-      end
-      its(:last) do
-        should == 'Bisco | 08/22/03 | Bay Center | Dewey Beach, DE | Setlist'
-      end
-    end
   end
 end
 
 describe Parse::Show do
-  let(:attributes) do
-    { date: '08/21/03',
-      venue: 'Crocodile Rock',
-      city: 'Allentown',
-      state: 'PA' }
+  let(:raw) do
+    'Bisco |  08/21/03 |  Crocodile Rock |  Allentown, PA |  Setlist'
   end
 
-  subject { described_class.new(attributes) }
+  subject { described_class.new(raw) }
 
+  its(:raw) do
+    should == 'Bisco | 08/21/03 | Crocodile Rock | Allentown, PA | Setlist'
+  end
   its(:venue) { should == 'Crocodile Rock' }
   its(:city) { should == 'Allentown' }
   its(:state) { should == 'PA' }
