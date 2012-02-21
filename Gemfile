b09be44a09acd456c336e803322c7c4b8b2d136a
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2012-02-21 14:43:10 -0600

    rails new mapz -O -T

--- Gemfile
+++ Gemfile
@@ -1,10 +1,16 @@
 source :rubygems
 
-gem 'activesupport'
 gem 'deep_struct'
+gem 'jquery-rails'
 gem 'rails', '3.2.1'
 
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
 group :development, :test do
-  gem 'rspec'
-  gem 'pry'
+  gem 'pry-rails'
+  gem 'rspec-rails'
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapz::Application.config.secret_token = '894d9aeb757473fc5d650cbdb4682ae78e6819ec26e5830024f972388e2d7160ed8a1d2e8faffb1c16ee04a51fe459199b60c7ca03f5d591bbf1c4f5fdfaf9aa'
