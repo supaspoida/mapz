c640e0db2ed622c6ea904a15505a8eda987b4ec7
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2012-02-24 18:23:47 -0500

    Move google geocoder into module
    * Compatibility with Rails auto loading conventions

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -3,88 +3,86 @@ require 'json'
 require 'deep_struct'
 require 'active_support/core_ext/object/try'
 
-class Google
-  class Error < RuntimeError
-  end
-  class Response
-    attr_reader :result, :status
-
-    def initialize(hsh)
-      mash = DeepStruct::Hash.new(hsh)
-      @status = mash.status
-      @result = mash.results.try(:first)
-      self
+module Geocoder
+  class Google
+    class Error < RuntimeError
     end
+    class Response
+      attr_reader :result, :status
 
-    def center
-      result.geometry.location if success?
-    end
+      def initialize(hsh)
+        mash = DeepStruct::Hash.new(hsh)
+        @status = mash.status
+        @result = mash.results.try(:first)
+        self
+      end
 
-    def southwest
-      if success? && viewport?
-        result.geometry.viewport.southwest
+      def center
+        result.geometry.location if success?
+      end
+
+      def southwest
+        if success? && viewport?
+          result.geometry.viewport.southwest
+        end
+      end
+
+      def northeast
+        if success? && viewport?
+          result.geometry.viewport.northeast
+        end
+      end
+
+      def viewport?
+        result.geometry.viewport.present?
+      end
+
+      def success?
+        status == 'OK'
+      end
+
+      def bounds
+        [[southwest.lat, southwest.lng],
+         [northeast.lat, northeast.lng]] if success?
+      end
+
+      def origin
+        [center.lat, center.lng] if success?
+      end
+
+      def city
+        find_address_component('sublocality').try(:long_name) ||
+        find_address_component('locality').try(:long_name)
+      end
+
+      def state
+        find_address_component('administrative_area_level_1').try(:short_name)
+      end
+
+      def to_mongo
+        { center: [center.try(:lat), center.try(:lng)],
+          city: city,
+          state: state,
+          status: status
+        }
+      end
+
+      protected
+      def find_address_component(type)
+        result.address_components.detect do |component|
+          component.types.include?(type)
+        end
       end
     end
 
-    def northeast
-      if success? && viewport?
-        result.geometry.viewport.northeast
-      end
+    def self.locate(address)
+      uri = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=#{address}"
+      response = Net::HTTP.get(URI.parse(URI.encode(uri)))
+      response = JSON.parse(response)
+      geo = Response.new(response)
+      raise Error, "#{response['status']} for #{address.inspect}" unless geo.success?
+      geo
     end
 
-    def viewport?
-      result.geometry.viewport.present?
-    end
-
-    def success?
-      status == 'OK'
-    end
-
-    def bounds
-      [[southwest.lat, southwest.lng],
-       [northeast.lat, northeast.lng]] if success?
-    end
-
-    def origin
-      [center.lat, center.lng] if success?
-    end
-
-    def city
-      find_address_component('sublocality').try(:long_name) ||
-      find_address_component('locality').try(:long_name)
-    end
-
-    def state
-      find_address_component('administrative_area_level_1').try(:short_name)
-    end
-
-    def to_mongo
-      { northeast: [northeast.try(:lat), northeast.try(:lng)],
-        southwest: [southwest.try(:lat), southwest.try(:lng)],
-        center: [center.try(:lat), center.try(:lng)],
-        city: city,
-        state: state,
-        status: status,
-        viewport: viewport?
-      }
-    end
-
-    protected
-    def find_address_component(type)
-      result.address_components.detect do |component|
-        component.types.include?(type)
-      end
-    end
   end
-
-  def self.locate(address)
-    uri = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=#{address}"
-    response = Net::HTTP.get(URI.parse(URI.encode(uri)))
-    response = JSON.parse(response)
-    geo = Response.new(response)
-    raise Error, "#{response['status']} for #{address.inspect}" unless geo.success?
-    geo
-  end
-
 end
-
